f4b0ca865d6a92ac57077b8335e694ff845aea2a
fullpwn <57821130+fullpwn@users.noreply.github.com>
2021-11-22 17:13:22 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ set -e -u -v
 start_time="$(date -u +%s)"
 
 # Install dependencies to build odysseyn1x
-apt-get update
+apt-get update --allow-unauthenticated
 apt-get install -y --no-install-recommends wget debootstrap grub-pc-bin \
     grub-efi-amd64-bin mtools squashfs-tools xorriso ca-certificates curl \
     libusb-1.0-0-dev gcc make gzip xz-utils unzip libc6-dev
